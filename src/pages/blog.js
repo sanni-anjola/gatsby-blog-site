380a56865f0460d4fd65035f465a62a13d632fed
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-12-25 18:17:42 +0100

    Added MDX plugin as a transformer plugin and to render mdx file

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,54 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from "../components/layout";
 const BlogPage = ({ data }) => {
   
 
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-
-      {data.allFile.nodes.map((el, ind) => {
-        return <li key={ind}>{el.name}</li>;
-      })}
-      </ul>
+            {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+                {node.body}
+            </MDXRenderer>
+          </article>
+        ))
+      }
     </Layout>
   );
 };
 
-export const query  = graphql`
-    query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+// export const query  = graphql`
+//     query {
+//     allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+//       nodes {
+//         name
+//       }
+//     }
+//   }`;
+
+export const query = graphql`
+query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
+        parent {
+          ... on File {
+            
+            modifiedTime(formatString: "MMMM D, YYYY @ HH:mm:ss")
+          }
+        }
       }
     }
-  }`;
+  }
+`
 export default BlogPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the gatsby tutorial</p>
       <StaticImage
-      src='https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8dGVjaG5vbG9neXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' 
-      // src='../images/abdullah-mongo-2.jpeg'
+      // src='https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8dGVjaG5vbG9neXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' 
+      src='../images/abdullah-mongo-2.jpeg'
       alt='A macbook pro'
       />
     </Layout>
