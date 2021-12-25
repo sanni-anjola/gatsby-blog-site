eb1e4772ac69675f0b151933d53aac98add3c5ba
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-12-25 15:22:23 +0100

    Added gatsby-plugin-image for adding responsive images to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby-blog-site",
   },
-  plugins: [],
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the gatsby tutorial</p>
+      <StaticImage
+      // src='https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8dGVjaG5vbG9neXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' 
+      src='../images/abdullah-mongo-2.jpeg'
+      alt='A macbook pro'
+      />
     </Layout>
   )
 }
